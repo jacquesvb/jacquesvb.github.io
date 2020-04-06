88ead527ee3a6400593b95221504616ee2b53403
Jacques Van Blokland <jacquesvb@live.com>
2020-04-05 20:53:32 -0700

    Updates

---
+++ precache-manifest.00b0fd14d61456986a89c9ff9317ddd7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b28f949d84d7dc1693ae89938bf7426b",
+    "url": "/jacquesvb.github.io/index.html"
+  },
+  {
+    "revision": "2983ad78920ab653e25e",
+    "url": "/jacquesvb.github.io/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d8653d271ef02354a369",
+    "url": "/jacquesvb.github.io/static/js/2.e310b272.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/jacquesvb.github.io/static/js/2.e310b272.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2983ad78920ab653e25e",
+    "url": "/jacquesvb.github.io/static/js/main.cfe09ee7.chunk.js"
+  },
+  {
+    "revision": "c1d3f4cf3b03b8416b17",
+    "url": "/jacquesvb.github.io/static/js/runtime-main.229c3366.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/jacquesvb.github.io/static/media/logo.5d5d9eef.svg"
+  }
+]);
